253c5a6c6201984ade8c72300aa2f84e64290062
yichunzhao <zhaoyichun@hotmail.com>
2020-06-17 17:34:57 +0200

    static css and view(data), controller return model and view;

---
+++ src/main/java/com/ynz/roombooking/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.ynz.roombooking.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurationSupport {
+}
--- src/main/java/com/ynz/roombooking/controller/RoomController.java
+++ src/main/java/com/ynz/roombooking/controller/RoomController.java
@@ -1,29 +1,28 @@
 package com.ynz.roombooking.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.ynz.roombooking.domain.Room;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Controller
 @RequestMapping("/rooms")
 public class RoomController {
-//    private static List<Room> rooms = new ArrayList<>();
-//
-//    static {
-//        IntStream.range(0, 10).forEach(i -> rooms.add(new Room(i, "room-" + i, "100" + i, "info: www" + i)));
-//    }
-//
-//    @GetMapping
-//    public String getAllRooms(Model model) {
-//        model.addAttribute("rooms", rooms);
-//        return "rooms";
-//    }
+    private static List<Room> rooms = new ArrayList<>();
 
-    @GetMapping("hello")
-    public ResponseEntity<String> helloWorld() {
-        return new ResponseEntity<>("hello world!", HttpStatus.OK);
+    static {
+        IntStream.range(0, 10).forEach(i -> rooms.add(new Room(i, "room-" + i, "100" + i, "info: w" + i)));
+    }
+
+    @GetMapping
+    public String getAllRooms(Model model) {
+        model.addAttribute("rooms", rooms);
+        return "rooms";
     }
 
 
